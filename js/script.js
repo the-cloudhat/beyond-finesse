801156ef06347225f487bb83aa677f129b384a8e
Aleksandar Babic <contact@aleksandar-babic.com>
2024-03-29 20:55:39 +0100

    Added Canva design

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-// This file is intentionally left blank.
-// Add your JavaScript code here to add interactivity to the webpage.
+var modal = document.getElementById("subscribeModal");
+var btn = document.getElementById("JtYhtYPiilVyOowg");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
