4b1c99bc8f8019e1c1e78581239f9aa886a726c6
Aleksandar Babic <contact@aleksandar-babic.com>
2024-03-29 20:04:01 +0100

    Added mailchimp verificaton

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-// This file is intentionally left blank.
-// Add your JavaScript code here to add interactivity to the webpage.
+!(function (c, h, i, m, p) {
+  (m = c.createElement(h)),
+    (p = c.getElementsByTagName(h)[0]),
+    (m.async = 1),
+    (m.src = i),
+    p.parentNode.insertBefore(m, p);
+})(
+  document,
+  "script",
+  "https://chimpstatic.com/mcjs-connected/js/users/ecb30718dfbeb7e5a32b777c6/7421e21e1216b8010c3fc9b42.js"
+);
